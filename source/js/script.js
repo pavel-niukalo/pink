a953941c1c58d34fc4d60668f6bc6cda0bd494f5
Pavel Nyukalo <PashaOktan07@gmail.com>
2019-12-31 11:41:06 +0500

    Завершает адаптивные сетки

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,4 @@
+// Открытие/закрытие меню на мобильном/планшете
 var header = document.querySelector(".page-header");
 var nav = document.querySelector(".main-nav");
 var navToggle = document.querySelector(".main-nav__toggle");
@@ -20,3 +21,47 @@ navToggle.addEventListener("click", function() {
 
   }
 });
+
+// Проверка формы и выдача попапов
+var form = document.querySelector("form");
+
+var popup = document.querySelector(".modal");
+var eror = document.querySelector(".modal--eror");
+var sent = document.querySelector(".modal--sent");
+var close = document.querySelector(".modal__button");
+var closeSent = document.querySelector(".modal__button--sent");
+var secondName = document.querySelector("[name=secondName]");
+var firstName = document.querySelector("[name=firstName]");
+var usersEmail = document.querySelector("[name=usersEmail]");
+
+form.addEventListener("submit", function (evt) {
+  if (!secondName.validity.valid || !firstName.validity.valid || !usersEmail.validity.valid) {
+    evt.preventDefault();
+    eror.classList.add("modal--show");
+  } else {
+    evt.preventDefault();
+    sent.classList.add("modal--show");
+  }
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  eror.classList.remove("modal--show");
+});
+
+closeSent.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  sent.classList.remove("modal--show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (eror.classList.contains("modal--show")) {
+      eror.classList.remove("modal--show");
+    } else {
+      sent.classList.remove("modal--show");
+    }
+  }
+});
+
