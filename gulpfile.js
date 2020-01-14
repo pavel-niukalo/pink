3203329e689b4f624404e8ed82792858351406f7
Pavel Nyukalo <PashaOktan07@gmail.com>
2020-01-14 16:41:17 +0500

    Добавляет сборку проекта, а так же изменения таблицы, и для IE11

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,16 @@
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
+var rename = require("gulp-rename");
+var del = require("del");
+var server = require("browser-sync").create();
 var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -16,14 +22,32 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style-min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
+gulp.task("images", function () {
+  return gulp.src("build/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"));
+})
+
+gulp.task("webp", function () {
+  return gulp.src("build/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"));
+  });
+
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -34,4 +58,23 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("clean", function () {
+  return del("build");
+})
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico",
+    "source/*.html"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("build", gulp.series("clean", "copy", "css", "images", "webp"));
+
+gulp.task("start", gulp.series("build", "server"));
---
+++ source/js/map.js
@@ -0,0 +1,27 @@
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('ymap', {
+      center: [59.93863223569252, 30.322965421264662],
+      zoom: 16,
+      controls: []
+    },{
+      suppressMapOpenBlock: true
+    },{
+      searchControlProvider: 'yandex#search'
+    }),
+    myPlacemark = new ymaps.Placemark(([59.93863223569252, 30.322965421264662]), {
+
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#image',
+      // Своё изображение иконки метки.
+      iconImageHref: 'img/icon-map-marker.svg',
+      // Размеры метки.
+      iconImageSize: [36, 36],
+      // Смещение левого верхнего угла иконки относительно
+      // её 'ножки' (точки привязки).
+      iconImageOffset: [0, 0]
+    });
+  myMap.geoObjects.add(myPlacemark);
+  myMap.options.set('scrollZoomSpeed', 1);
+});
